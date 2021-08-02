9cce3e154e5076972dbceaee57e30262926f53ce
WTS007 <85397632+WTS007@users.noreply.github.com>
2021-08-02 12:36:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 #install
 chmod u+x timer.sh
 chmod u+x 2nd.sh
+chmod u+x timer1.sh
+cp 2nd.sh ~
+echo "Do you have terminator (1 for Yes & 0 for No)? "
+read ans
+if [ $ans == 1 ]
+then
+  cp timer.sh ~
+fi
+if [ $ans == 0 ]
+then
+  cp timer1.sh ~
+fi
 path=$pwd
 cd ~
 export PATH="$PATH:$path"
