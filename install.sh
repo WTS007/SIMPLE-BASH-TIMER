a4af89b243497984e3656c43fc6d48a33863bfad
WTS007 <85397632+WTS007@users.noreply.github.com>
2021-08-03 01:42:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 chmod u+x timer.sh
 chmod u+x 2nd.sh
 chmod u+x timer1.sh
+chmod +x custom_cmd.sh
 cp 2nd.sh ~
 echo "Do you have terminator (1 for Yes & 0 for No)? "
 read ans
@@ -13,6 +14,12 @@ fi
 if [ $ans == 0 ]
 then
   cp timer1.sh ~
+fi
+echo "Do you plan on using the Custom Command in Gnome-Terminal for timer (1 for Yes & 0 for No) ? "
+read c
+if [ $c == 1 ]
+then
+  cp custom_cm.sh ~
 fi
 path=$pwd
 cd ~
